32ea7ded1fde8556a7c089da777d0af3de6ae9d6
Budoya <budoyya@gmail.com>
2024-05-01 23:55:22 -0300

    decorator

---
+++ decorators.ts
@@ -0,0 +1,17 @@
+/*
+ao usar o decoretor ha de se atualizar o arquivo config.ts para liberar os decorators adicionando; "experimentaldecorators":true
+decorators sao ferramentas que acrescentao funcionalidades extras aos codigos
+tipos de decorators 
+
+//Class Decorators: Aplicados a classes, permitem modificar a estrutura e o comportamento da classe como um todo.
+
+//Method Decorators: Aplicados a métodos, interceptam a execução do método e podem ser utilizados para diversas tarefas, 
+como validação de argumentos, registro de logs ou injeção de dependências.
+
+//Property Decorators: Aplicados a propriedades, interceptam o acesso à propriedade e podem ser utilizados para validação de dados, 
+formatação de valores ou inicialização automática de propriedades.
+
+//Parameter Decorators: Aplicados a parâmetros de métodos, permitem validar os valores dos argumentos passados para o método.
+
+para usar o decoretor ha de se entender
+*/
